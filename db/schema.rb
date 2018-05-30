e2daec5bc823a1b9bca61b0447a1b945db8b4b7e
fyi2 <tony@sherman.org>
2018-05-30 11:01:02 -0400

    Finished Comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_29_150714) do
+ActiveRecord::Schema.define(version: 2018_05_29_181341) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "comment"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
